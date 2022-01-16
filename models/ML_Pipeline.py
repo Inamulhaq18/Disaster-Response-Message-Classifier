d9a0eaa69e759f58bfafbed1923efb00a9e3f912
Inamulhaq18 <haqinamul18@gmail.com>
2022-01-17 02:52:51 +0300

    Update ML_Pipeline.py

--- models/ML_Pipeline.py
+++ models/ML_Pipeline.py
@@ -62,7 +62,7 @@ def build_model():
                 'clf__estimator__n_estimators': [10, 25],
                 'clf__estimator__min_samples_split': [2, 4]}
 
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+    cv = GridSearchCV(pipeline, param_grid=parameters) #using random forest as the classifier
 
     return cv
     
